b0a044cb0053da4ad50011d70ab628014bad2a5e
Shivani Kapania <shivani.kapania@gmail.com>
2019-04-14 23:53:17 +0530

    First commit

---
+++ Extract_face_regions.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from collections import OrderedDict
+import numpy as np
+import cv2
+import dlib
+from facial_landmark_detections import rect_to_bb, shape_to_np, resize
+
+
+# Examining the image, we can see that facial regions can be accessed via simple Python indexing (assuming zero-indexing with Python since the image above is one-indexed):
+# 
+# The mouth can be accessed through points [48, 68].
+# The right eyebrow through points [17, 22].
+# The left eyebrow through points [22, 27].
+# The right eye using [36, 42].
+# The left eye with [42, 48].
+# The nose using [27, 35].
+# And the jaw via [0, 17].
+# These mappings are encoded inside the FACIAL_LANDMARKS_IDXS  dictionary
+
+# In[2]:
+
+
+#dictionary that maps the indexes of the facial landmarks to specific face regions
+
+#For dlib’s 68-point facial landmark detector:
+FACIAL_LANDMARKS_68_IDXS = OrderedDict([
+    ("mouth", (48, 68)),
+    ("inner_mouth", (60, 68)),
+    ("right_eyebrow", (17, 22)),
+    ("left_eyebrow", (22, 27)),
+    ("right_eye", (36, 42)),
+    ("left_eye", (42, 48)),
+    ("nose", (27, 36)),
+    ("jaw", (0, 17))
+])
+
+
+# In[3]:
+
+
+FACIAL_LANDMARKS_IDXS = FACIAL_LANDMARKS_68_IDXS
+
+
+# In[4]:
+
+
+def visualize_facial_landmarks(image, shape, colors=None, alpha=0.75):
+    # create two copies of the input image -- one for the
+    # overlay and one for the final output image
+    overlay = image.copy()
+    output = image.copy()
+
+    # if the colors list is None, initialize it with a unique
+    # color for each facial landmark region
+    if colors is None:
+        colors = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
+            (168, 100, 168), (158, 163, 32),
+            (163, 38, 32), (180, 42, 220), (0,255,255)]
+
+    # loop over the facial landmark regions individually
+    for (i, name) in enumerate(FACIAL_LANDMARKS_IDXS.keys()):
+        # grab the (x, y)-coordinates associated with the face landmark
+        (j, k) = FACIAL_LANDMARKS_IDXS[name]
+        pts = shape[j:k]
+
+        # check if are supposed to draw the jawline
+        if name == "jaw":
+            # since the jawline is a non-enclosed facial region,
+            # just draw lines between the (x, y)-coordinates
+            pass
+            for l in range(1, len(pts)):
+                ptA = tuple(pts[l - 1])
+                ptB = tuple(pts[l])
+                cv2.line(overlay, ptA, ptB, colors[i], 2)
+
+        # otherwise, compute the convex hull of the facial
+        # landmark coordinates points and display it
+        else:
+            hull = cv2.convexHull(pts)
+            cv2.drawContours(overlay, [hull], -1, colors[i], -1)
+
+    # apply the transparent overlay
+    cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+
+    # return the output image
+    return output
+
+
+# In[5]:
+
+
+for (i, name) in enumerate(FACIAL_LANDMARKS_IDXS.keys()):
+        # grab the (x, y)-coordinates associated with the
+        # face landmark
+        print(str(i)+" "+name)
+
+
+# In[6]:
+
+
+FACIAL_LANDMARKS_IDXS['mouth']
+
+
+# In[7]:
+
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+
+# In[8]:
+
+
+# load the input image, resize it, and convert it to grayscale
+image = cv2.imread('example_01.jpeg')
+image = resize(image, width=500)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ 
+# detect faces in the grayscale image
+rects = detector(gray, 1)
+
+
+# In[9]:
+
+
+# loop over the face detections
+for (i, rect) in enumerate(rects):
+    # determine the facial landmarks for the face region, then
+    # convert the landmark (x, y)-coordinates to a NumPy array
+    shape = predictor(gray, rect)
+    shape = shape_to_np(shape)
+ 
+    # loop over the face parts individually
+    for (name, (i, j)) in FACIAL_LANDMARKS_IDXS.items():
+        # clone the original image so we can draw on it, then
+        # display the name of the face part on the image
+        clone = image.copy()
+        cv2.putText(clone, name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+ 
+        # loop over the subset of facial landmarks, drawing the
+        # specific face part
+        for (x, y) in shape[i:j]:
+            cv2.circle(clone, (x, y), 1, (0, 0, 255), -1)
+        # extract the ROI of the face region as a separate image
+        (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
+        roi = image[y:y + h, x:x + w]
+        roi = resize(roi, width=250, inter=cv2.INTER_CUBIC)
+ 
+        #show the particular face part
+        cv2.imshow("ROI", roi)
+        cv2.imshow("Image", clone)
+        cv2.waitKey(0)
+ 
+    # visualize all facial landmarks with a transparent overlay
+    output = visualize_facial_landmarks(image, shape)
+    cv2.imshow("Image", output)
+    cv2.waitKey(0)
+
+
+# In[19]:
+
+
+rects
+
+
+# In[ ]:
+
+
+
+
---
+++ facial_landmark_detections.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from collections import OrderedDict
+import numpy as np
+import cv2
+import dlib
+
+
+# In[2]:
+
+
+def rect_to_bb(rect):
+    # take a bounding predicted by dlib and convert it
+    # to the format (x, y, w, h) as we would normally do
+    # with OpenCV
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+
+    # return a tuple of (x, y, w, h)
+    return (x, y, w, h)
+
+
+# In[3]:
+
+
+def shape_to_np(shape, dtype="int"):
+    # initialize the list of (x, y)-coordinates
+    coords = np.zeros((shape.num_parts, 2), dtype=dtype)
+
+    # loop over all facial landmarks and convert them
+    # to a 2-tuple of (x, y)-coordinates
+    for i in range(0, shape.num_parts):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+
+    # return the list of (x, y)-coordinates
+    return coords
+
+
+# In[4]:
+
+
+#resize while preserving the aspect ration
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+
+
+
+
+
